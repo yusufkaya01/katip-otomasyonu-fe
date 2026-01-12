9fe2587ce1cf8f420091644b588bd70b2c3bdd3b
yusufkayatr96 <yusufkayater96@gmail.com>
2026-01-12 10:34:17 +0300

    copyright upgrade++

--- src/pages/CopyrightPage.js
+++ src/pages/CopyrightPage.js
@@ -19,7 +19,7 @@ function CopyrightPage() {
           Üçüncü parti kütüphaneler ilgili lisansları ile kullanılır ve bu lisanslara uyulur. Kullanıcılar, uzantıyı ve web sitesini yalnızca kendi kurumları için kullanabilir, ticari olarak satamaz, çoğaltamaz veya dağıtamaz. Katip Otomasyonu'nun marka ve telif hakları Arkaya Arge Yazılım İnşaat Ticaret Ltd.Şti.'nin izni olmadan kullanılamaz.
         </p>
         <p>
-          Sitede yer alan ticari unvan, marka, logo, alan adı ve benzeri diğer tüm unsur ve içeriklerin fikrî ve sınai mülkiyet hakları veya kullanım lisansı Arkaya Arge Yazılım İnşaat Ticaret Ltd.Şti.'ye aittir. Kullanıcıya yalnızca şahsi kullanım ile sınırlı, münhasır olmayan, devredilemez ve değiştirilemez bir kullanım lisansı verilmektedir. Sitenin tamamı veya bir kısmı, herhangi bir yöntemle kopyalanamaz, çoğaltılamaz, dağıtılamaz, yayımlanamaz, ödünç verilemez, içeriği değiştirilemez veya tahrip edilemez.
+          Sitede yer alan ticari unvan, marka, logo, alan adı ve Katip Otomasyonu uzantısı ile benzeri diğer tüm unsur ve içeriklerin fikrî ve sınai mülkiyet hakları veya kullanım lisansı Arkaya Arge Yazılım İnşaat Ticaret Ltd.Şti.'ye aittir. Kullanıcıya yalnızca şahsi kullanım ile sınırlı, münhasır olmayan, devredilemez ve değiştirilemez bir kullanım lisansı verilmektedir. Sitenin tamamı veya bir kısmı ve Katip Otomasyonu uzantısı, herhangi bir yöntemle kopyalanamaz, çoğaltılamaz, dağıtılamaz, yayımlanamaz, ödünç verilemez, içeriği değiştirilemez veya tahrip edilemez.
         </p>
         <p className="mb-4">
           Detaylı bilgi için info@arkaya.com.tr adresine başvurabilirsiniz.
